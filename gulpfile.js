f9dd83e7735b4a1a1fc67942e4f30a879ed78660
Dawa Sherpa <dasherpa7@gmail.com>
2017-09-22 12:32:35 -0400

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 
-
 gulp.task('browserSync', function() {
   browserSync.init({
+    open: false,
     server: {
       baseDir: 'app'
     },
@@ -12,7 +12,7 @@ gulp.task('browserSync', function() {
 
 gulp.task('watch', ['browserSync'], function (){
   // Reloads the browser whenever HTML, CSS, or JS files change
-  gulp.watch('app/*.css', browserSync.reload); 
+  gulp.watch('app/css/*.css', browserSync.reload); 
   gulp.watch('app/*.html', browserSync.reload); 
   gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
